3da1d57fe16674e80a49f2a32e024d4265dd25a7
sanjiv576 <shresthasanjiv576@gmail.com>
2024-06-25 19:58:02 +0545

    add constant colors

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class KColorsConstants {
+  KColorsConstants._();
+
+  static const Color progressBarColor = Color(0xFFff5c00);
+  static const Color buttonColor = Color(0xFFff9200);
+  static const Color lowestTextColor = Color(0xFF383838);
+  static const Color lowerTextColor = Color(0xFF686868);
+  static const Color lowTextColor = Color(0xFFc7c7c7);
+  static const Color whiteTextColor = Colors.white;
+  static const Color backgroundColor = Color(0xFF202020);
+}
